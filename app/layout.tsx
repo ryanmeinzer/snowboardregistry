23efee31bc7e4c976db642776eb7217de82fc51d
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2024-06-11 04:40:40 +0000

    added legacy files and updated og share image

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,11 @@ export const metadata: Metadata = {
   keywords: ['snowboard registry', 'snowboard theft prevention', 'snowboard recovery', 'snowboard serial number', 'stolen snowboards', 'lost snowboards', 'snowboard registration', 'snowboard tracking', 'snowboard protection', 'snowboard identification'],
   creator: 'Ryan Meinzer',
   description: 'Secure your snowboard with SnowboardRegistry.com. Prevent theft and recover lost or stolen boards with our serial number tracking and identification system.',
-  // openGraph: {
-  //   images: [{
-  //     url: "/splash-2.jpg",
-  //   }],
-  // },
+  openGraph: {
+    images: [{
+      url: "/share-image.png",
+    }],
+  },
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
