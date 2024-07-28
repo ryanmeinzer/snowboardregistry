be5dc3b3ca706b1bc8b7be0d8721802064557bac
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2024-07-28 15:01:22 -0700

    resolved page imports

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,8 +4,6 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import NextLink from 'next/link';
-import ProTip from '@/components/ProTip';
-import Copyright from '@/components/Copyright';
 
 export default function About() {
   return (
@@ -27,8 +25,6 @@ export default function About() {
             Go to the home page
           </Button>
         </Box>
-        <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
